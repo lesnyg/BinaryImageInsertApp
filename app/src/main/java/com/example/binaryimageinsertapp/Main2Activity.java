28f17745b0682b5706fe24d96ef68c07d33100de
lesnyg <lsy79@hanmail.net>
2020-01-06 16:32:50 +0900

    바이너리 이미지 저장 및 보기

---
+++ app/src/main/java/com/example/binaryimageinsertapp/Main2Activity.java
@@ -0,0 +1,87 @@
+package com.example.binaryimageinsertapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Base64;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class Main2Activity extends AppCompatActivity {
+    private ImageView img_result;
+    private AsyncTask<String, String, String> mTask;
+    private Bitmap bitmap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        img_result = findViewById(R.id.img_result);
+        mTask = new MySyncTask().execute();
+
+    }
+    public class MySyncTask extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            if (isCancelled())
+                return null;
+            listQuery();
+            return null;
+
+        }
+
+        protected void onPostExecute(String result) {
+        }
+
+        protected void onCancelled() {
+            super.onCancelled();
+        }
+
+    }
+
+    private void listQuery() {
+        Connection connection;
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:jtds:sqlserver://222.122.213.216/mashw08", "mashw08", "msts0850op");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from Su_배너이미지 where id='21'");
+            byte b[];
+            while (resultSet.next()) {
+                Blob blob = resultSet.getBlob(2);
+                b = blob.getBytes(1, (int) blob.length());
+                bitmap = BitmapFactory.decodeByteArray(b, 0, b.length);
+
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        img_result.setImageBitmap(bitmap);
+
+
+                    }
+                });
+            }
+            connection.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/binaryimageinsertapp/MainActivity.java
@@ -0,0 +1,144 @@
+package com.example.binaryimageinsertapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AsyncTask<String, String, String> bnAsyncTask;
+    private String mImage;
+    private ImageView img_binery;
+
+    private Bitmap mBitmap;
+    private byte[] imageBytes;
+    private ByteArrayOutputStream baos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        img_binery = findViewById(R.id.img_binery);
+//        BitmapDrawable drawable = (BitmapDrawable) getResources().getDrawable(R.drawable.ad01);
+//        mBitmap = ((BitmapDrawable)drawable).getBitmap();
+        mBitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.ad06);
+
+        img_binery.setImageBitmap(mBitmap);
+        BitmapToString(mBitmap);
+
+
+        findViewById(R.id.btn_save).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bnAsyncTask = new bnAsyncTask().execute();
+            }
+        });
+
+        findViewById(R.id.btn_send).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+            }
+        });
+    }
+
+    public class bnAsyncTask extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            if (isCancelled())
+                return (null);
+            query();
+
+
+            return null;
+        }
+
+        protected void onPostExecute(String result) {
+        }
+
+        protected void onCancelled() {
+            super.onCancelled();
+        }
+    }
+
+    private void query() {
+        Connection connection = null;
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:jtds:sqlserver://222.122.213.216/mashw08", "mashw08", "msts0850op");
+//            PreparedStatement ps = connection.prepareStatement("INSERT INTO Su_배너이미지(BLOBData) VALUES (convert(VARBINARY(max),'" + imageBytes + "'))");
+            PreparedStatement ps = connection.prepareStatement("INSERT INTO Su_배너이미지(BLOBData) VALUES (?)");
+//            ps.setString(1, file.getName());
+            byte[] buf = imageBytes;
+            ps.setBytes(1, buf);
+            ps.executeUpdate();
+            ps.close();
+            connection.close();
+        }  catch (SQLException e) {
+            e.printStackTrace();
+        }  catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+//        try {
+//
+//            String query = "insert into Su_배너이미지(BLOBData) VALUES (convert(VARBINARY(max),'" + imageBytes + "'))";
+//            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+//            connection = DriverManager.getConnection("jdbc:jtds:sqlserver://222.122.213.216/mashw08", "mashw08", "msts0850op");
+//            Statement statement = connection.createStatement();
+//            ResultSet resultSet = statement.executeQuery("insert into Su_배너이미지(BLOBData) VALUES (convert(VARBINARY(max),'" + imageBytes + "'))");
+//            PreparedStatement pstm=connection.prepareStatement(query);
+//
+//            fis=new FileInputStream(file);
+//            pstm.setBinaryStream(2, (InputStream)fis, (int)file.length());
+//            pstm.executeUpdate();
+//            pstm.close();
+//            fis.close();
+//            connection.close();
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+    }
+
+    public void BitmapToString(Bitmap bitmap) {
+        baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);    //bitmap compress
+        imageBytes = baos.toByteArray();
+//        mImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+
+    }
+}
